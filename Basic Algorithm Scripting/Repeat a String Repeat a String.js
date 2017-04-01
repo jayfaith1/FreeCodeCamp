b2684f8ef79acab92c8656c6dd6b4d44cdacff4d
James Andeson <jayfaith1Dev@gmail.com>
2017-04-01 10:08:20 -0400

    Complete Repeat a string repeat a string
    Repeat a given string (first argument) num times (second argument).
    Return an empty string if num is not a positive number.

---
+++ Basic Algorithm Scripting/Repeat a String Repeat a String.js
@@ -0,0 +1,9 @@
+//Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  if (num < 0)
+    return "";
+  return str.repeat(num);
+}
+
+repeatStringNumTimes("abc", 3);
